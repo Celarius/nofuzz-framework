6d73d872da5d315232254a4dbd292d05ba7f1d1b
Kim Sandell <Kim Sandell>
2017-03-18 19:45:33 +0100

    UUID v4 and v5 added to UUID helper

--- src/Helpers/UUIDInterface.php
+++ src/Helpers/UUIDInterface.php
@@ -16,4 +16,29 @@ interface UUIDInterface
    * @return string
    */
   public static function generate(): string;
+
+  /**
+   * Generate v4 UUID
+   *
+   * @return string
+   */
+  public static function v4(): string;
+
+  /**
+   * Generate a v5 UUID, based on $namespace and $name
+   *
+   * @param  string $namespace    A Valid UUID
+   * @param  string $name         A Random String
+   * @return string
+   */
+  public static function v5(string $namespace, string $name): string;
+
+  /**
+   * Checks if an UUID is valid (v3,v4 and v5)
+   *
+   * @param  string  $uuid
+   * @return bool
+   */
+  public static function is_valid(string $uuid): bool;
+
 }
