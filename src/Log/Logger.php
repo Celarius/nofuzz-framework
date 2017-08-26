6a15d8d44264712e7ec4c45f5b036531a592d1c5
Kim Sandell <sandell@celarius.com>
2017-08-26 14:35:57 +0200

    Logger comments updated

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -1,11 +1,18 @@
 <?php
 /**
- * composer require monolog/monolog
+ * Nofuzz Logger
  *
+ * PSR-3 compliant Logger wrapper
+ *
+ * @package   Nofuzz
  */
+#################################################################################################################################
 
 namespace Nofuzz\Log;
 
+/**
+ * Logger - PSR-3 Compliant Logger wrapper
+ */
 class Logger extends \Monolog\Logger
 {
 }
